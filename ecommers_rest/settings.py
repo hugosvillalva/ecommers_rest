4e79347b9714fff89d87b257e198ba9e27d75ca5
AL3M4ND1Y3 <hugosebastianvillalva@gmail.com>
2023-04-14 22:52:38 -0300

    test-login

--- ecommers_rest/settings.py
+++ ecommers_rest/settings.py
@@ -171,7 +171,6 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
